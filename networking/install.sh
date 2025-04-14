9c2593e7c6c12157ba3ec4f31f380e2eff64c822
Ethan Li <lietk12@gmail.com>
2025-04-14 14:49:42 -0700

    Set up installation of Tailscale

--- networking/install.sh
+++ networking/install.sh
@@ -26,3 +26,16 @@ if command -v raspi-config &>/dev/null; then
 else
   echo "Warning: raspi-config is not available, so we can't set the wifi country!"
 fi
+
+# Install tailscale
+sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
+  apt-transport-https
+DISTRO_VERSION_CODENAME="$(. /etc/os-release && echo "$VERSION_CODENAME")"
+curl -fsSL "https://pkgs.tailscale.com/stable/raspbian/$DISTRO_VERSION_CODENAME.gpg" |
+  sudo apt-key add -y -
+curl -fsSL "https://pkgs.tailscale.com/stable/raspbian/$DISTRO_VERSION_CODENAME.list" |
+  sudo tee /etc/apt/sources.list.d/tailscale.list >/dev/null
+sudo -E apt-get update -y -o Dpkg::Progress-Fancy=0 # get the list of packages from the docker repo
+sudo -E apt-get install -y -o Dpkg::Progress-Fancy=0 \
+  sudo apt-get install tailscale
+
